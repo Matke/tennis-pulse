865c1f1ac3f9839fc5bda881accd7acade7f2f8d
Matke <matijaguitar@gmail.com>
2025-11-16 18:14:37 +0100

    Small fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import AppTest from "./test/components/source/AppTest";
-
 function App() {
   return (
     <>
       <div className="text-amber-400">Tennis Pulse App</div>
-      <AppTest />
     </>
   );
 }
