0b4419d2d1b9f11382a3909057ddded954ba4210
Matke <matijaguitar@gmail.com>
2025-11-26 19:07:51 +0100

    Small class fix

--- src/pages/LoginLayout.tsx
+++ src/pages/LoginLayout.tsx
@@ -32,12 +32,12 @@ const LoginLayout = () => {
 
                 <div className="absolute inset-0 animate-pulse rounded-full border border-white/10"></div>
 
-                <div className="rounded-full border border-white/20 bg-gradient-to-br from-black/80 to-black/60 p-6 shadow-2xl backdrop-blur-lg transition-all duration-500 group-hover:scale-110 group-hover:rotate-12">
+                <div className="rounded-full border border-white/20 bg-linear-to-br from-black/80 to-black/60 p-6 shadow-2xl backdrop-blur-lg transition-all duration-500 group-hover:scale-110 group-hover:rotate-12">
                   <div className="transition-transform duration-700">
                     <img
                       src={newLogo}
                       alt=""
-                      className="scale-170 animate-pulse transition-all duration-300 hover:scale-200"
+                      className="scale-[2] animate-pulse transition-all duration-300 hover:scale-[3]"
                     />
                   </div>
                 </div>
