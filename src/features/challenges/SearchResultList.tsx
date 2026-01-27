a47819bfd13490c39508028bd1fa7e3b721dc983
Matke <matijaguitar@gmail.com>
2026-01-27 19:38:06 +0100

    Fix

--- src/features/challenges/SearchResultList.tsx
+++ src/features/challenges/SearchResultList.tsx
@@ -2,9 +2,20 @@ import SearchResult from "@/features/challenges/SearchResult";
 
 const SearchResultList = () => {
   return (
-    <div className="bg-tp-card-back shadow-tp-primary/20 absolute bottom-12 left-0 z-100 flex h-50 w-full flex-col rounded-lg p-3 shadow-xs">
+    <div className="bg-tp-card-back shadow-tp-primary/20 scrollbar-hide absolute bottom-12 left-0 z-100 flex h-50 w-full flex-col overflow-y-auto rounded-lg p-3 shadow-xs">
+      {/* <div className="max-h-60 overflow-y-auto"> inside div scroll */}
       <SearchResult />
       <SearchResult />
+      <SearchResult />
+      <SearchResult />
+      <SearchResult />
+      <SearchResult />
+
+      <SearchResult />
+      <SearchResult />
+      <SearchResult />
+      <SearchResult />
+      {/* </div> */}
     </div>
   );
 };
